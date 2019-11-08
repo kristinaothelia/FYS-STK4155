8821ad8a9e074405528e33eee2df19c3268cb986
kwsgd <42683129+kwsgd@users.noreply.github.com>
2019-11-08 17:39:06 +0100

    seaborn

--- Project2/main.py
+++ Project2/main.py
@@ -163,8 +163,10 @@ elif arg == "NN":
 
     Y_train_onehot, Y_test_onehot = to_categorical_numpy(y_train), to_categorical_numpy(y_test)
 
-    epochs = 30
-    batch_size = 500
+
+    # 78 accuracy
+    epochs = 100 #60 #30
+    batch_size = 80 #60 #500
 
     eta_vals = np.logspace(-7, -4, 7)
     #eta_vals = np.linspace(0, 3.5, 7)
@@ -199,124 +201,8 @@ elif arg == "NN":
             
 
     np.save('acc_score', accuracy_array)
-
-    """
-	eta = 0.01        #1e-4
-	gamma = 0.0   #0.01
-
-	n_features 		 = len(X[0])
-	n_hidden_neurons = 29 # one layer?
-	n_categories 	 = 29 # ? 
-	n_inputs         = 80 # ?
-	epochs           = 1000
-	iterations       = 1000
-	batch_size       = 80
-
-
-	a_h, probabilities = NN.feed_forward_train(X, n_features, n_hidden_neurons, n_categories)
-
-	print(a_h)
-	print(probabilities)
-	print(np.min(probabilities))
-
-	acc_feed_forwrd_train = func.accuracy(probabilities[:,0], y)
-
-	print(acc_feed_forwrd_train, 'acc')
-
-	prob, output_weights_grad, output_bias_grad, hidden_weights_grad, hidden_bias_gradi = NN.train(X, y, eta, gamma, n_inputs, epochs, iterations, batch_size, n_features, n_hidden_neurons, n_categories)
-	print(prob)
-
-	prob = np.array(prob)
-	print(prob.shape)   # why this shape??
-	acc_train = func.accuracy(prob[1,1,1], y)
-	print(acc_train)
-	
-	predict = NN.predict(X, y, eta, gamma, n_features, n_hidden_neurons, n_categories) 
-	predict_probability = NN.predict_probabilities(X, y, eta, gamma, n_features, n_hidden_neurons, n_categories)
-    """
-
-
-'''
-nn = NeuralNetwork(X, y)
-
-a_h, probabilities = nn.feed_forward()
-acc_feed_forwrd_train = func.accuracy(a_h, y)
-print(acc_feed_forwrd_train)
-
-pp = nn.predict_probabilities(nn.X)
-print(pp)
-
-acc_predict = func.accuracy(pp, y)
-print(acc_predict)
-'''
-
-'''
-cost = func.cost(X_test, y_train, betas_train)   # ytilde or model??
-print(cost)
-plt.plot(cost)
-plt.show()
-'''
-
-
-'''
-plt.plot(epoch_list, acc_train, label='train')
-plt.plot(epoch_list, acc_test, label='test')
-plt.legend()
-plt.show()
-'''
-
-
-
-
-'''
-for e in range(len(eta_range)):
-	for g in range(len(gamma_range)):
-
-		print('----------')
-		print(eta_range[e])
-		print(gamma_range[g])
-		print('----------')
-
-		# Calculating the beta values based og the training set
-		betas_train = func.next_beta(X_train, y_train, eta_range[e], gamma_range[g])
-		#betas_train = func.steepest(X_train, y_train, gamma)
-
-		# Calculating ytilde and the model of logistic regression 
-		z 		    = X_test @ betas_train   # choosing best beta here? 
-		model       = func.logistic_function(z)
-
-		# Calculating the accuracy with our own function
-		accuracy_test =  func.accuracy(model, y_test)
-		exp_term = X_test
-		Probabilities = func.probabilities(exp_term)   # ??? 
-
-		# Creating a logistic regression model with scikit-learn 
-		# Calculating the corresponding accuracy
-		logReg = LogisticRegression(random_state=0, solver='sag', max_iter=1000, fit_intercept=False) # solver='lbfgs'
-		logReg.fit(X_train, np.ravel(y_train))
-
-		ypredict_scikit  		     = logReg.predict(X_test)
-		predict_probabilities_scikit = logReg.predict_proba(X_test)  # Probability estimates
-		score_scikit				 = logReg.score(X_test, y_test)  # ?? same as accuracy ?? 
-
-		accuracy_scikit  = accuracy_score(y_pred=ypredict_scikit, y_true=y_test)
-
-		# Comparing our own accuracy with scikit-learn
-		print('')
-		'-------------------------------------------'
-		print('The accuracy with our function is  :', accuracy_test)
-		print('The accuracy of scikit-learn is    :', accuracy_scikit)
-		'-------------------------------------------'
-
-		fpr, tpr, thresholds = roc_curve(y_test, predict_probabilities_scikit[:,1], pos_label=None)
-		AUC_scikit 			 = auc(fpr, tpr)
-
-		# The AUC scikit
-		print('')
-		'-------------------------------------------'
-		print('The AUC is:', AUC_scikit)
-		'-------------------------------------------'
-'''
+    np.save('eta_values', eta_vals)
+    np.save('lambda_values', lmbd_vals)
 
 
 
@@ -327,42 +213,19 @@ for e in range(len(eta_range)):
 
 
 
-#logReg= SGDClassifier(loss='log', random_state=seed, eta0=eta, learning_rate='constant', fit_intercept=False, max_iter=1000)
-
-#sc = StandardScaler()
-#robust_scaler = RobustScaler() transform
 
 
-# Should we use Regressor or Classifier ??
-
-'''
-sgdreg = SGDRegressor(random_state=seed, eta0=eta, epsilon=1e-8)
-#sgdreg = SGDRegressor(max_iter = 1000, eta0=eta, alpha=gamma, random_state=seed) # average=?
-sgdreg.fit(X,np.ravel(y))
-#print(sgdreg.intercept_, sgdreg.coef_)
-print(sgdreg.coef_)
-print('-------')
-'''
-
-#sgd_clas = SGDClassifier(loss='log', random_state=seed, eta0=eta, learning_rate='constant', fit_intercept=False, max_iter=1000)
-#sgd_clas.fit(X,np.ravel(y))
-#print(sgd_clas.coef_)
-#print('-------')
 
 
-#beta_linreg = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
-#print(beta_linreg)
 
-'''
-# Create an instance of the estimator
-#sc = StandardScaler()
-logReg = LogisticRegression(solver='lbfgs', max_iter=1000) #n_jobs=-1, random_state=15 # multi_class='multinomial'
-# Using the training data to train the estimator
-logReg.fit(X, np.ravel(y))
-betas_logreg = logReg.coef_
-print(betas_logreg)
-'''
 
+
+
+
+
+
+
+    
 
 
 
--- Project2/map.py
+++ Project2/map.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+sns.set()
 
-arr = np.load('acc_score.npy', allow_pickle=True)
+arr     = np.load('acc_score.npy', allow_pickle=True)
+etas    = np.load('eta_values.npy', allow_pickle=True)
+lambdas = np.load('lambda_values.npy', allow_pickle=True)
 
 new_arr = np.zeros(shape=arr.shape)
 
@@ -12,9 +16,18 @@ for i in range(Ni):
     for j in range(Nj):
         new_arr[i][j] = arr[i][j]
 
-plt.imshow(new_arr, origin="lower")
-plt.colorbar()
+
+
+ax = sns.heatmap(new_arr, xticklabels=lambdas, yticklabels=etas, annot=True, linewidths=.3)
+plt.title('Accuracy')
 plt.ylabel('eta')
 plt.xlabel('lambda')
 plt.show()
 
+
+#plt.style.use('dark_background')
+#plt.imshow(new_arr, origin="lower")
+#plt.colorbar()
+#plt.ylabel('eta')
+#plt.xlabel('lambda')
+#plt.show()
