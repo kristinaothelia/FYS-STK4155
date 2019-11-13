7e0e4d36f22021fe5ee32743324a17c733c2a693
Kristina Othelia <koolsen@student.matnat.uio.no>
2019-11-13 17:32:37 +0100

    Updated NN linreg

--- Project2/main.py
+++ Project2/main.py
@@ -19,6 +19,7 @@ import plots             as P
 import functions         as func
 import nn_linreg_sklearn as NN_Franke_sklearn
 from   neural_network    import NN
+from   nn_linreg_new     import MLP
 
 # -----------------------------------------------------------------------------
 seed = 0
@@ -136,7 +137,170 @@ if __name__ == '__main__':
 	    print('The AUC is       : %.3f' % AUC_scikit)
 	    print('-------------------------------------------')
 
-	elif arg == "linreg":
+    elif arg == "linreg":
+
+		if __name__ == "__main__":
+
+		    def FrankeFunction(x,y):
+			    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+			    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+			    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+			    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+			    return term1 + term2 + term3 + term4
+
+		    def create_X(x, y, n ):
+			    if len(x.shape) > 1:
+				    x = np.ravel(x)
+				    y = np.ravel(y)
+			    N = len(x)
+			    l = int((n+1)*(n+2)/2)		# Number of elements in beta
+			    X = np.ones((N,l))
+			    for i in range(1,n+1):
+				    q = int((i)*(i+1)/2)
+				    for k in range(i+1):
+					    X[:,q+k] = (x**(i-k))*(y**k)
+			    return X
+
+		    def relu(x):
+		        """
+		        the relu function
+		        f(x) = max(0, x)
+		        """
+		        return abs(x)*(x > 0)
+
+		    def idf(x):
+		        """
+		        the identity function
+		        f(x) = x
+		        """
+		        return x
+
+		    def d_relu(x):
+		        return 1.*(x > 0)
+
+
+			np.random.seed(5)
+
+			n = 5  	 # Project 1
+			N = 200  # Project 1
+			k = 20   # Project 1
+
+			x = np.sort(np.random.uniform(0, 1, N))
+			y = np.sort(np.random.uniform(0, 1, N))
+			X = func.create_X(x, y, n)
+
+			XX, YY = np.meshgrid(x,y)
+			ZZ     = func.FrankeFunction(XX, YY)
+			z      = func.Create_data(XX, YY, ZZ, noise=True)
+			X      = func.create_X(XX, YY, n)
+
+			X_train, X_test, y_train, y_test = train_test_split(X, z, test_size=1.0/k)
+
+			epochs     = 500 # 1000
+			batch_size = 100
+
+		    X_train, X_test = X_train.T, X_test.T
+
+		    eta_vals  = np.logspace(-6, -3, 4)
+		    lmbd_vals = np.logspace(-6, -3, 4)
+		    MSE_array = np.zeros((len(eta_vals), len(lmbd_vals)))  #keeping the MSE value for heatmap
+
+		    n_features = len(X_train)  # nr of predictors
+		    n_responses = 1
+		    n_nodes = 50
+		    hidden_activation = relu
+		    response_activation = idf
+		    hidden_activation_df = d_relu
+
+		    MSE = np.zeros((len(eta_vals),len(lmbd_vals)))
+
+		    nn_reg = MLP(n_features=n_features, n_responses=n_responses, n_nodes=n_nodes,
+		                 hidden_activation=hidden_activation, response_activation=response_activation,
+		                 hidden_activation_df=hidden_activation_df)
+
+		    nn_reg.create_biases_and_weights(weight_spread = 0.1, bias_constant = 0.1)
+
+		    def not_now():
+		        for i,eta in enumerate(eta_vals):
+		            for j,lmbd in enumerate(lmbd_vals):
+
+		                print("step:    ", i, j)
+		                print("eta    = ", eta)
+		                print("lambda = ", lmbd)
+
+		                nn_reg.train(X_train, y_train, learning_rate=eta, regularization=lmbd,
+		                             n_epochs=epochs, batch_size=batch_size)
+		                y_pred = nn_reg.predict(X_test).flatten()
+
+		                MSE[i,j] = mean_squared_error(y_train, y_pred)
+
+		                print("MSE score on test set: ", MSE[i,j])
+		                print()
+
+		    not_now()
+
+		    np.save('acc_score', accuracy_array)
+		    np.save('eta_values', eta_vals)
+		    np.save('lambda_values', lmbd_vals)
+
+			P.map()
+
+
+			'''
+			etas = ["{:0.2e}".format(i) for i in eta_vals]
+
+			fig, ax = plt.subplots()
+			sns.heatmap(MSE, annot=True, xticklabels=lmbd_vals, yticklabels=etas, ax=ax, linewidths=.3, linecolor="black")
+			ax.set_title("MSE scores (sklearn)")
+			ax.set_ylabel("$\eta$")
+			ax.set_xlabel("$\lambda$")
+			plt.show()
+
+			fig, ax = plt.subplots()
+			sns.heatmap(R2, annot=True, xticklabels=lmbd_vals, yticklabels=etas, ax=ax, linewidths=.3, linecolor="black")
+			ax.set_title("Accuracy/R2 scores (sklearn)")
+			ax.set_ylabel("$\eta$")
+			ax.set_xlabel("$\lambda$")
+			plt.show()
+			'''
+
+			# Find best values:
+
+		    i_min, j_min = 0,0
+
+		    nn_reg.train(X_train, y_train, learning_rate=eta_vals[-1], regularization=lmbd_vals[-1],
+		                 n_epochs=epochs, batch_size=batch_size)
+		    y_pred2 = nn_reg.predict(X_train).flatten()
+
+		    print("MSE sco  re on test set: ", mean_squared_error(y_train, np.ravel(y_pred2)))
+
+
+			fig  = plt.figure(); fig2 = plt.figure()
+			#cmap = cm.PRGn; my_cmap_r = cm.get_cmap('PRGn_r')
+			ax   = fig.gca(projection='3d'); ax2  = fig2.gca(projection='3d')
+
+			ax.set_title("Franke's function", fontsize=16)
+			ax2.set_title("Model (sklearn)", fontsize=16)
+
+			# Plot the surface.
+			surf = ax.plot_surface(XX, YY, ZZ, cmap=cm.inferno, #my_cmap_r
+			                       linewidth=0, antialiased=False)
+
+			surf2 = ax2.plot_surface(XX, YY, y_pred2, cmap=cm.inferno,
+			                       linewidth=0, antialiased=False)
+
+			# Customize the z axis.
+			ax.view_init(azim=61, elev=15);  ax2.view_init(azim=61, elev=15)
+			ax.set_xlabel('x',  fontsize=15); ax.set_ylabel('y',  fontsize=15); ax.set_zlabel('z',  fontsize=15)
+			ax2.set_xlabel('x', fontsize=15); ax2.set_ylabel('y', fontsize=15); ax2.set_zlabel('z', fontsize=15)
+
+			# Add a color bar which maps values to colors.
+			fig.colorbar(surf,   shrink=0.5, aspect=5)
+			fig2.colorbar(surf2, shrink=0.5, aspect=5)
+			plt.show()
+
+
+	elif arg == "linreg_sklearn":
 
 		#NN_Franke_sklearn.Heatmap_MSE_R2()
 		NN_Franke_sklearn.Best_model()
---
+++ Project2/nn_linreg_new.py
@@ -0,0 +1,112 @@
+import numpy as np
+import sys
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import plots        as P
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+from random import random, seed
+
+class MLP:
+    def __init__(self, n_features, n_responses, n_nodes,
+                 hidden_activation, response_activation,
+                 hidden_activation_df):
+        """
+        n_features           | number of features
+        n_responses          | number of response variables
+        n_nodes              | number of nodes in hidden layer
+        hidden_activation    | activation function for hidden layer
+        response_activation  | activation function for ouput layer
+        """
+        # store attributes
+        # important: store activation functions as attributes
+        # setup weights and biases
+        self.n_features = n_features
+        self.n_responses = n_responses
+        self.n_nodes = n_nodes
+        self.hidden_activation = hidden_activation
+        self.response_activation = response_activation
+        self.hidden_activation_df = hidden_activation_df
+
+    def create_biases_and_weights(self, weight_spread=0.1, bias_constant=0.1):
+        """
+        create inital biaes and weights
+        Gaussian distribution, mean=?, std=?
+        """
+        self.hidden_weights = weight_spread*np.random.randn(self.n_features, self.n_nodes).T
+        self.hidden_bias = np.zeros((self.n_nodes,1)) + bias_constant
+        self.output_weights = weight_spread*np.random.randn(self.n_nodes, self.n_responses).T
+        self.output_bias = np.zeros((self.n_responses,1)) + bias_constant
+
+    def train(self, X, Y, learning_rate, regularization, n_epochs, batch_size):
+
+        self.learning_rate = learning_rate
+        self.regularization = regularization
+        self.n_epochs = n_epochs
+        self.batch_size = batch_size
+
+        if Y.shape[0] != self.n_responses:
+            Y = Y.T if len(Y.shape) == 2 else Y[:,None].T
+
+        n_inputs = X.shape[1]
+        data_indices = np.arange(n_inputs)
+        n_batches = int((X.shape[1]/self.batch_size))
+        #print(self.batch_size)
+        #print(n_batches)
+        for i in range(self.n_epochs):
+            for j in range(n_batches):
+                # prepare minibatch
+                chosen_datapoints = np.random.choice(data_indices, size=self.batch_size, replace=False)
+
+                idx = chosen_datapoints
+                X_batch, Y_batch = X[:,idx], Y[:,idx]
+
+                # learning algorithm
+                Y_predict = self.predict(X_batch)
+                self.backpropagation(Y_batch, Y_predict, X_batch)
+                if np.any(np.isnan(self.hidden_weights)):
+                    sys.exit(1)
+                else:
+                    pass#print(self.hidden_weights)
+
+    def predict(self, X):
+        # hidden layer matrix multiplication
+        self.z_h = np.matmul(self.hidden_weights, X) + self.hidden_bias
+        """if np.any(self.z_h > 10):
+            sys.exit(1)
+        else:
+            print(self.z_h)"""
+        # hidden layer activation ( e.g. A_hidden = self.hidden_activation(z_h) )
+        self.a_h = self.hidden_activation(self.z_h)
+
+        # output layer matrix multiplication
+        z_o = np.matmul(self.output_weights, self.a_h) + self.output_bias
+
+        # output layer activation
+        #a_o = self.response_activation(z_o)
+        #prediction = a_o
+        return z_o#prediction
+
+    def backpropagation(self, Y_true, Y_predict, X):
+        # backpropagate prediction error
+        error_output = Y_true - Y_predict
+        error_hidden = np.matmul(self.output_weights.T, error_output) * self.hidden_activation_df(self.z_h)
+
+        # compute weight gradients
+        output_weights_gradient = np.matmul(error_output, self.a_h.T)
+        output_bias_gradient = np.sum(error_output, axis=1)[:,None]
+
+        hidden_weights_gradient = np.matmul(error_hidden, X.T)
+        hidden_bias_gradient = np.sum(error_hidden, axis=1)[:,None]
+
+        if self.learning_rate > 0.0:
+            output_weights_gradient -= self.regularization * self.output_weights
+            hidden_weights_gradient -= self.regularization * self.hidden_weights
+        self.output_weights += self.learning_rate * output_weights_gradient
+        self.output_bias    += self.learning_rate * output_bias_gradient
+        self.hidden_weights += self.learning_rate * hidden_weights_gradient
+        self.hidden_bias    += self.learning_rate * hidden_bias_gradient
