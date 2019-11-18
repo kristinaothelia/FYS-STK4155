176f1fcaa51b000f0fff28be076e0a6d03fbe1b9
Kristina Othelia <koolsen@student.matnat.uio.no>
2019-11-18 20:27:56 +0100

    Hist test

--- Project3/exo_data.py
+++ Project3/exo_data.py
@@ -10,7 +10,7 @@ filename = cwd + '/cumulative_2019_all.xls'
 nanDict  = {}
 df       = pd.read_excel(filename, header=1, skiprows=85, index_col=0, na_values=nanDict)
 
-# Check DONE/ACTIVE, if ACTIVE, drop this 
+# Check DONE/ACTIVE, if ACTIVE, drop this
 
 # Removing the same columns as mentioned by the scientific article, do we want to drop more features or not?
 df.drop(columns=['koi_longp', 'koi_ingress', 'koi_model_dof', 'koi_model_chisq', 'koi_sage'], axis=1, inplace=True)
@@ -24,29 +24,58 @@ print(df)
 
 '''
 # Dropping NaN values or not? If so, how??
-# Asking Morten about this? 
+# Asking Morten about this?
 df = df.drop(df[(df.koi_dikco_mra  == 'NaN')].index)
 df = df.drop(df[(df.koi_dikco_mdec == 'NaN')].index)
 df = df.drop(df[(df.koi_dikco_msky == 'NaN')].index)
 '''
 
+def Corr_matrix():
+	#Creating a correlation matrix for the dataframe
+	# Why is this different from the paper? Is this a problem?
+	sns.heatmap(df.corr())
+	plt.title("Correlation Matrix")
+	plt.tight_layout()
+	plt.show()
 
-#Creating a correlation matrix for the dataframe, why is this different from the paper? Is this a problem?
-sns.heatmap(df.corr())
-plt.title("Correlation Matrix")
-plt.tight_layout()
-plt.show()
-
+#Corr_matrix()
 
 # Locking the Confirmed, False Positive and Candidates (using this for plotting histogram of distribution?)
 # The Candidates, later used for calculating the probability that a candidate is an exoplanet
-CONFIRMED  = df.loc[df['koi_disposition']  == 'CONFIRMED']       # = 1 
+
+df = df.drop(df[(df.koi_prad > 15)].index)		# TEST
+
+CONFIRMED  = df.loc[df['koi_disposition']  == 'CONFIRMED']       # = 1
 NEGATIVE   = df.loc[df['koi_disposition']  == 'FALSE POSITIVE']  # = 0
 CANDIDATES = df.loc[df['koi_disposition']  == 'CANDIDATE']
-print(CONFIRMED)
-print(NEGATIVE)
-print(CANDIDATES)
 
+#print(CONFIRMED)
+#print(NEGATIVE)
+#print(CANDIDATES)
+
+def Histogram(feature_name, x_label):
+	'''
+	fig, ax = plt.subplots()
+	labels, counts = np.unique(NEGATIVE.loc[:,  NEGATIVE.columns == feature_name].values, return_counts=True)
+	plt.bar(labels, counts)
+	labels2, counts2 = np.unique(CONFIRMED.loc[:,  CONFIRMED.columns == feature_name].values, return_counts=True)
+	plt.bar(labels2, counts2)
+	labels3, counts3 = np.unique(CANDIDATES.loc[:,  CANDIDATES.columns == feature_name].values, return_counts=True)
+	plt.bar(labels3, counts3)
+	'''
+	plt.hist(CONFIRMED.loc[:,  CONFIRMED.columns == feature_name].values, alpha=0.8, bins=50, label="Confirmed")
+	plt.hist(NEGATIVE.loc[:,   NEGATIVE.columns == feature_name].values, alpha=0.8, bins=100, label="False positive")
+	plt.hist(CANDIDATES.loc[:, CANDIDATES.columns == feature_name].values, alpha=0.8, bins=100, label="Candidates")
+
+	plt.title("Histogram for Kepler exoplanet data")
+	plt.xlabel(x_label)
+	plt.ylabel("Observations count")
+	plt.xlim(0, 15)
+	plt.legend()
+	plt.show()
+
+# Make histogram for planet radius [Earth radii], koi_prad
+Histogram('koi_prad', "Planet radius [Earth radii], koi_prad")
 
 # Creating DataFrame only with the Confirmed/False Positive (dropping the candidates)
 # The features (not including the column koi_disposition or the Kepler ID)
@@ -63,6 +92,3 @@ target[target == 'CONFIRMED']      = 1
 target[target == 'FALSE POSITIVE'] = 0
 
 print(target)
-
-
-
