91da124ff0e9d5677ade425dac464782fc0fac8c
aronjn <lagnadslaus@gmail.com>
2019-12-08 19:42:14 +0100

    lalala

--- Project3/xgboost_test.py
+++ Project3/xgboost_test.py
@@ -3,12 +3,16 @@ import xgboost as xgb
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import precision_score, recall_score, accuracy_score, mean_squared error, mean_absolute_error
+from sklearn.metrics import precision_score, recall_score, accuracy_score, mean_squared_error, mean_absolute_error
 
 X = np.load("features.npy", allow_pickle=True)
 y = np.load("targets.npy", allow_pickle=True)
+
 y = y.astype('int')
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.3)
+
+print("Shape of training data:", X_train.shape)
+print("Shape of testing data: ", X_test.shape)
 
 D_train = xgb.DMatrix(X_train, label=Y_train)
 D_test = xgb.DMatrix(X_test, label=Y_test)
@@ -26,6 +30,7 @@ best_preds = np.asarray([np.argmax(line) for line in preds])
 
 #model.dump_model('test.txt')
 
+print(50*"-")
 print("Precision = {}".format(precision_score(Y_test, best_preds, average="macro")))
 print("Recall = {}".format(recall_score(Y_test, best_preds, average="macro")))
 print("Accuracy = {}".format(accuracy_score(Y_test, best_preds)))
